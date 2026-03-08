a9f9013f0c28f77f0a9dd4a0c14bac17d4936c84
sammid37 <samanthadmedeiros37@gmail.com>
2026-03-07 22:46:22 -0300

    Add App Module and Tests

---
+++ backend/src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { PokemonsModule } from './pokemons/pokemons.module';
+import { Pokemon } from './pokemons/entities/pokemon.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [User, Pokemon],
+        // TODO: quando estiver em produção, trocar por false e utilizar migrations
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UsersModule,
+    PokemonsModule,
+  ],
+})
+export class AppModule {}

6ad93fb712218c57955f613990442ae1d7db9178
sammid37 <samanthadmedeiros37@gmail.com>
2026-03-07 22:45:50 -0300

    Add main file

---
+++ backend/src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  
+  app.useGlobalFilters(new HttpExceptionFilter());
+  
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Centro Pokémon API')
+    .setDescription('API para gerenciamento de pokémons do Centro Pokémon')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
+}
+
+bootstrap();

65406f0a786a43c7e2924f10813dccdff227119d
sammid37 <samanthadmedeiros37@gmail.com>
2026-03-09 00:08:19 -0300

    Update payload to return user name

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -16,7 +16,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 		});
 	}
 
-	async validate(payload: { sub: string; email: string; role: string }) {
-		return { id: payload.sub, email: payload.email, role: payload.role };
+	async validate(payload: { sub: string; email: string; role: string; name: string }) {
+		return { id: payload.sub, email: payload.email, role: payload.role, name: payload.name };
 	}
 }
